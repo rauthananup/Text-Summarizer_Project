02070502bd2478e22fae71c0e830f2ba70ed1a2e
Anup Rauthan <rauthananup03@gmail.com>
2024-08-04 18:35:08 +0530

    git commit -m requirements

---
+++ setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+with open("README.md","r", encoding="utf-8") as f:
+    long_description = f.read()
+    
+    
+__version__="0.0.0"
+
+REPO_NAME = "Text-Summarizer_Project"
+AUTHOR_USER_NAME = "rauthananup"
+SRC_REPO = "textSummarizer"
+AUTHER_EMAIL = "rauthanan03@gmail.com"
+
+
+setuptools.setup(
+    name = SRC_REPO,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHER_EMAIL,
+    description="A small python package for NLP app",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    drl=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_urls={
+        "BugTracker":f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+        
+    },
+    package_dir = {"": "src"},
+    packages=setuptools.find_packages(where="src")
+    
+)
